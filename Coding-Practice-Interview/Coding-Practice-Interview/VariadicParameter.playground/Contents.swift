8a7dc60fd209f9e5ee889e0fb1bc93eda1013d4c
ajeet2012 <ajeetmca2012@gmail.com>
2026-04-26 16:20:50 +0530

    Coding: Variadic parameter

---
+++ Coding-Practice-Interview/Coding-Practice-Interview/VariadicParameter.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+/* - Variadic parameter
+ 
+ A variadic parameter allows a function to accept zero or more values of the same type.
+ 
+ */
+
+//MARK: -  sumOfNumbers
+func sumOfNumbers(_ numbers: Int...) -> Int {
+    var total = 0
+    for number in numbers {
+        total += number
+    }
+    return total
+}
+
+print(sumOfNumbers(1, 2, 3, 4, 5))
+print(sumOfNumbers())
+
+//MARK: -  1. Only one variadic parameter per function
+
+//func feature(_ numbers1: Int..., _ numbers2: Int...){
+       // ❌ Not allowed
+//}
+
+
+//MARK: -  2. Variadic parameter must be last parameter
+//func feature(_ numbers: Int..., _ text: String){
+    // ❌ Not allowed
+//}
+
+//MARK: - 3 . Can combine with normal parameters
+
+func greet(message: String, _ names: String...) {
+    names.forEach { print("\(message) \($0)") }
+}
+
+greet(message: "Good morning", "Alice", "Bob", "Charlie")
