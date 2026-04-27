5339dabf4b9a359a2139df795019991d049fa3d1
ajeet2012 <ajeetmca2012@gmail.com>
2026-04-27 14:14:03 +0530

    Comobine: Subject in Combine framework

--- ReactiveFramework/ReactiveFramework/LoginViewModel.swift
+++ ReactiveFramework/ReactiveFramework/LoginViewModel.swift
@@ -8,6 +8,35 @@
 import Foundation
 import Combine
 
+/*
+ 
+ Subject :
+ 
+ A Subject is a special publisher that allows manual emission of values using send() and can act as both publisher and subscriber.
+ 
+ # PassthroughSubject vs CurrentValueSubject
+ 
+ 1. PassthroughSubject:
+ 
+ 
+ let subject = PassthroughSubject<[Int], Never>()
+ 
+ •    ❌ No initial value
+ •    ❌ Doesn’t store last value
+ •    ✅ Good for events (API response, button tap)
+ 
+ 
+ 
+ 2. CurrentValueSubject:
+ 
+ •    ✅ Has initial value
+ •    ✅ Stores latest value
+ •    ✅ New subscribers get last value immediately
+ 
+ 
+ 
+ */
+
 
 enum NetworkError: Error {
     case noDataAvailable
@@ -15,7 +44,7 @@ enum NetworkError: Error {
 }
 
 class LoginViewModel {
-     
+    
     //These are Publishers
     @Published var isLoading: Bool = false
     @Published var error: NetworkError? = nil
